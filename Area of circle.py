186fdf266e23cfe860ccf08ce1eee0c4d4d17682
Shashank26112 <90169675+Shashank26112@users.noreply.github.com>
2021-11-14 12:44:01 +0530

    Area of circle.py

--- Area of circle.py
+++ Area of circle.py
@@ -1,5 +1,9 @@
 # area-of-circle
 PI = 3.14
-r = float(input("Enter the radius of a circle:"))
+print ("enter 'x' for exit:")
+rad = input("Enter the radius of a circle:")
+If rad =='x':
+Else 
+Radius = float(rad)
 area = PI * r * r
-print("Area of a circle = %0.2f" %area)
+print("\n Area of a circle = %0.2f" %area);
