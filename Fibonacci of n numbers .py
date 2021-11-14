59d27992f332ff071cac98f1879f17533ff3005b
Shashank26112 <90169675+Shashank26112@users.noreply.github.com>
2021-11-14 12:58:07 +0530

    Fibonacci of n Number.py

---
+++ Fibonacci of n numbers .py
@@ -0,0 +1,19 @@
+Take input: the number of terms from the user
+N terms =int(input("how many terms ?"))
+# First two terms 
+n1=0
+n2=0
+count=2
+#check if the numbers of terms is valid 
+If n terms <=0;
+Print(" enter a positive integer")
+Elif n terms=1
+print (' Fibonacci sequence:")
+print(n1)
+Else 
+print(" Fibonacci sequence:")
+print(n!",", end =',')
+While = count< n terms;
+n+n=n1+n2
+print(n th end=',')
+Count+=1 
